cab60dbf2b4cc01126dac02e4f402e6e772093eb
Viraj Hapaliya <virajhapaliya25@gmail.com>
2023-02-16 11:40:07 +0530

    - Added different mode for different model

--- README.md
+++ README.md
@@ -4,15 +4,32 @@ On give image it will generate depth image.
 
 # Output Samples
 
+## "Hybrid" Model Ouptut
+
 <p float="left">
   <img src="Input\test1.jpg" width="280" />
-  <img src="output\test1_depth.jpg" width="280" /> 
+  <img src="output\test1_hybrid_depth.jpg" width="280" /> 
 </p>
 <p float="right">
   <img src="Input\test2.jpg" width="280" />
-  <img src="output\test2_depth.jpg" width="280" /> 
+  <img src="output\test2_hybrid_depth.jpg" width="280" /> 
 </p>
 <p float="left">
   <img src="Input\test3.jpg" width="280" />
-  <img src="output\test3_depth.jpg" width="280" /> 
+  <img src="output\test3_hybrid_depth.jpg" width="280" /> 
+</p>
+
+## "Large" Model Ouptut
+
+<p float="left">
+  <img src="Input\test1.jpg" width="280" />
+  <img src="output\test1_large_depth.jpg" width="280" /> 
+</p>
+<p float="right">
+  <img src="Input\test2.jpg" width="280" />
+  <img src="output\test2_large_depth.jpg" width="280" /> 
+</p>
+<p float="left">
+  <img src="Input\test3.jpg" width="280" />
+  <img src="output\test3_large_depth.jpg" width="280" /> 
 </p>
--- depth_estimation.py
+++ depth_estimation.py
@@ -9,20 +9,28 @@ import argparse
 import os
 
 
-def load_model(low_cpu_mem_usage=True):
-    model = DPTForDepthEstimation.from_pretrained("Intel/dpt-hybrid-midas", low_cpu_mem_usage=low_cpu_mem_usage)
-    feature_extractor = DPTFeatureExtractor.from_pretrained("Intel/dpt-hybrid-midas")
-    return model,feature_extractor
+def load_model(model:str,low_cpu_mem_usage=True):
+    if model=='hybrid':
+        model = DPTForDepthEstimation.from_pretrained("Intel/dpt-hybrid-midas", low_cpu_mem_usage=low_cpu_mem_usage)
+        feature_extractor = DPTFeatureExtractor.from_pretrained("Intel/dpt-hybrid-midas")
+        return model,feature_extractor
+    elif model=='large':
+        model = DPTForDepthEstimation.from_pretrained("Intel/dpt-large")
+        feature_extractor = DPTFeatureExtractor.from_pretrained("Intel/dpt-large")
+        return model,feature_extractor
 
-def generate_image(model,feature_extractor,image_name:str,
-                    output_folder:str,show_image=True,save=False):
+
+
+def generate_image(model_mode,image_name:str,
+                    output_folder:str,show_image=True,save=False,):
+    model,feature_extractor = load_model(model_mode)
     
     if save:
         if not os.path.isdir(output_folder):
             os.makedirs(output_folder)
         image_basename = os.path.basename(image_name).split('.')[0]
-        depth_output_imagepath = os.path.join(output_folder,f"{image_basename}_depth.jpg")
-        contacted_depth_output_imagepath = os.path.join(output_folder,f"{image_basename}_concate.jpg")
+        depth_output_imagepath = os.path.join(output_folder,f"{image_basename}_{model_mode}_depth.jpg")
+        contacted_depth_output_imagepath = os.path.join(output_folder,f"{image_basename}_{model_mode}_concate.jpg")
 
     image = Image.open(image_name)
     image_org  =cv2.imread(image_name)
@@ -68,10 +76,9 @@ if __name__ == "__main__":
     parser.add_argument('-output', help='Enter Output Folder name', type=str,default='output')
     parser.add_argument('-show_image',help="Show output image",type=bool,default=False)
     parser.add_argument('-save',help="If user want to save the image or not",type=bool,default=False)
+    parser.add_argument('-model',help="If user want to save the image or not",choices=['hybrid','large'],default='hybrid')
+
+    
 
     args = parser.parse_args()
-
-    model,feature_extractor = load_model()
-
-
-    generate_image(model,feature_extractor,args.input,args.output,args.show_image,args.save)
+    generate_image(args.model,args.input,args.output,args.show_image,args.save)
